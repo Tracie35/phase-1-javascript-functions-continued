5f9626241f7a9d5bf32f3f97ee508ae4ac37b17f
Tracie35 <kariukitracie@gmail.com>
2022-06-08 10:36:35 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+const saturdayFun = (place ='roller-skate')=> `This Saturday, I want to ${place}!` ;
+const mondayWork = (place = 'go to the office')=> `This Monday, I will ${place}.`;
+function wrapAdjective(wrap){
+    return function (def='special'){
+        return `You are ${wrap}${def}${wrap}!`
+    }
+}
